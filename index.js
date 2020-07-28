3a06508fe1132e52a9ca2dcd08b475c0c1b2c8b6
itfibonacci <itfibonacci@live.com>
2020-07-28 15:26:33 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+
+
+app.get("/", (req, res) => {
+    res.send("Users Shown");
+});
+
+app.get("/delete", (req, res) => {
+    res.send("Delete User");
+});
+
+app.get("/update", (req, res) => {
+    res.send("Update User");
+});
+
+app.get("/insert", (req, res) => {
+    res.send("Insert User");
+});
+
+app.listen(3000, function () {
+    console.log("listening on 3000");
+});
